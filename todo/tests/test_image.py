52a731693786cddc641b2c2ba0f52b53d6da97cb
cjy <sapla@naver.com>
2026-03-06 16:32:20 +0900

    test: add signup, login, logout and ownership test cases

--- todo/tests/test_image.py
+++ todo/tests/test_image.py
@@ -1,6 +1,16 @@
 import pytest
 from django.urls import reverse
 
+"""
+DRF DefaultRouter 자동 생성 URL 이름 (basename="todo")
+todo:todo-list         → GET  /todo/viewsets/view/         (목록 조회)
+todo:todo-list         → POST /todo/viewsets/view/         (생성)
+todo:todo-detail       → GET  /todo/viewsets/view/{pk}/   (단일 조회)
+todo:todo-detail       → PUT  /todo/viewsets/view/{pk}/   (전체 수정)
+todo:todo-detail       → PATCH /todo/viewsets/view/{pk}/  (부분 수정)
+todo:todo-detail       → DELETE /todo/viewsets/view/{pk}/ (삭제)
+"""
+
 
 @pytest.mark.django_db
 class TestTodoImage:
